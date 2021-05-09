132e91cb54e50b3d63b5333857dfbd1fbee43919
arubasu06 <80857448+arubasu06@users.noreply.github.com>
2021-05-09 12:14:51 +0900

    Add files via upload

--- view.py
+++ view.py
@@ -8,7 +8,7 @@ size=(800,800)
 
 @ eel.expose
 def main(csv_name,buy_item_code,buy_item_count,change_money):
-    search.main(csv_path,csv_name,buy_item_code,buy_item_count,change_money)
+    search.main(csv_name,buy_item_code,buy_item_count,change_money)
 
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
