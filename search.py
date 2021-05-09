ee57754f3e52cefaf6b164eb8aed9fc8bd1c7d08
arubasu06 <80857448+arubasu06@users.noreply.github.com>
2021-05-09 17:11:07 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -3,13 +3,15 @@ import eel
 import sys
 import datetime
 
-ITEM_MASTER_CSV_PATH="./item_master.csv"
 RECEIPT_FOLDER="./receipt"
+
+item_order_list=[]
+item_count_list=[]
 
 class Food_MenuItem:
     def __init__(self, item_code, item_name, price):
         self.item_code = item_code
-        self.item_name = item_name
+        self.item_name = item_name        
         self.price = price
 
     def info(self):
@@ -17,82 +19,63 @@ class Food_MenuItem:
 
 class Order:
     def __init__(self,item_master):
-        self.item_order_list=[]
-        self.item_count_list=[]
         self.item_master=item_master
         self.set_datetime()
     
     def set_datetime(self):
         self.datetime=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
-    def add_item_order(self, item_code, item_count):
-        self.item_order_list.append(item_code)
-        self.item_count_list.append(item_count)
-
-    def view_item_list(self):
-        for item in self.item_order_list:
-            print("商品コード:{}".format(item))
-
-    def get_item_data(self,item_code):
+    def get_item_data(self,buy_item_code):
             for m in self.item_master:
-                if item_code==m.item_code:
+                if buy_item_code==m.item_code:
                     return m.item_name,m.price
 
-    def input_order(self):
-        print("いらっしゃいませ！")
-        while True:
-            buy_item_code=input("商品番号を入力。注文を完了する場合は、0を入力してください >>> ")
-            if int(buy_item_code)!=0:
-                check=self.get_item_data(buy_item_code)
-                if check!=None:
-                    print("{} が注文されました".format(check[0]))
-                    buy_item_count=input("購入数を入力してください　>>> ")
-                    self.add_item_order(buy_item_code,buy_item_count)
-                    eel.buy_item_view_js("{0} が {1} 個注文登録されました".format(buy_item_code,buy_item_count))
-                else:
-                    print("「{}」は商品MENUに存在しません".format(buy_item_code))
-                    eel.buy_item_view_js("{}は商品MENUに存在しません")
-            else:
-                print("商品注文を終了します")
-                #eel.buy_item_view_js("商品注文を終了します")
-                break
+    def input_order(self,buy_item_code,buy_item_count):
+        item_info=self.get_item_data(buy_item_code)
+        eel.buy_item_view_js("{0[0]} ({0[1]} 円) が {1} 個注文されました".format(item_info,buy_item_count))
+        global item_order_list
+        global item_count_list
+        item_order_list.append(buy_item_code)
+        item_count_list.append(buy_item_count)
     
     def view_order(self):
         number=1
-        self.sum_price=0
-        self.sum_count=0
+        global sum_price
+        sum_price=0
+        global sum_count
+        sum_count=0
         self.receipt_name="receipt_{}.log".format(self.datetime)
         self.write_receipt("-----------------------------------------------")
         self.write_receipt("注文商品一覧\n")
-        for item_order,item_count in zip(self.item_order_list,self.item_count_list):
-            result=self.get_item_data(item_order)
-            self.sum_price+=result[1]*int(item_count)
-            self.sum_count+=int(item_count)
-            receipt_data="{0}.{2}({1}) : ￥{3:,}　{4}個 = ￥{5:,}".format(number,item_order,result[0],result[1],item_count,int(result[1])*int(item_count))
+        for item_order,item_count in zip(item_order_list,item_count_list):     
+            result = self.get_item_data(item_order)
+            sum_price+=result[1]*int(item_count)
+            sum_count += int(item_count)
+            receipt_data="{0}.{2}({1}) : ￥{3:,}　{4}個 = ￥{5:,}".format(number,item_order,result[0],result[1],item_count,int(result[1])*int(item_count))  
             self.write_receipt(receipt_data)
-            #eel.buy_item_view_js("{0}.{2}({1}) : ￥{3:,}　{4}個 = ￥{5:,}".format(number,item_order,result[0],result[1],item_count,int(result[1])*int(item_count)))
+            #購入個数、購入金額出力
+            eel.pay_money_js("{0}.{2}({1}) : ￥{3:,}　{4}個 = ￥{5:,}".format(number,item_order,result[0],result[1],item_count,int(result[1])*int(item_count)))
             number+=1
-
         # 合計金額、個数の表示
         self.write_receipt("-----------------------------------------------")
-        self.write_receipt("合計金額:￥{:,} {}個".format(self.sum_price,self.sum_count))
-        eel.pay_money_js("合計金額:￥{:,} 購入数{}個".format(self.sum_price,self.sum_count))
+        self.write_receipt("合計金額:￥{:,} {}個".format(sum_price,sum_count))
+        #最終的な合計金額出力
+        eel.pay_money_js("-----------------------------------------------")
+        eel.pay_money_js("合計金額:￥{:,} 購入数{}個".format(sum_price,sum_count))
+        eel.pay_money_js("-----------------------------------------------")
 
-    def input_change_money(self,change_money):
-        while True:
-            self.money = input("投入金額を入力してください >>> ")
-            self.change_money = int(self.money) - self.sum_price
-            if self.change_money>=0:
-                print("投入金額は" + str(self.money) + '円です')
-                print("お釣りは" + str(self.change_money) + '円です')
-                eel.change_js("お釣りは" + str(self.change_money) + '円です')
-                break
-            else:
-                print("お預かり金が不足しています。再度入力してください")
-                eel.change_js("お預かり金が不足しています。再度入力してください")
-
-        print("お買い上げありがとうございます")
-        eel.change_js("お買い上げありがとうございます")
+    def input_change_money(self,pay_money):
+        change_money = int(pay_money) - sum_price
+        eel.change_money_js("お預かり：{}円".format(pay_money))
+        if change_money>=0:
+            print("投入金額は" + str(pay_money) + '円です')
+            print("お釣りは" + str(change_money) + '円です')
+            eel.change_money_js("お釣り  ：{}円".format(change_money))
+            print("お買い上げありがとうございます!")
+            eel.change_money_js("お買い上げありがとうございます!")
+        else:
+            print("お預かり金が不足しています。再度入力してください")
+            eel.change_money_js("お預かり金が不足しています。再度入力してください")
 
     def write_receipt(self,text):
         print(text)
@@ -101,6 +84,7 @@ class Order:
 
 def add_item_master_by_csv(csv_path):
     print("-----------------------")
+    global item_master
     item_master=[]
     count=0
     try:
@@ -111,7 +95,7 @@ def add_item_master_by_csv(csv_path):
             eel.menu_view_js("{}:{}円  商品番号:{}".format(item_name,price,item_code))
             count+=1
         print("{}品の登録を完了。".format(count))
-        eel.menu_view_js("-----------{}品の登録を完了-----------".format(count))
+        eel.menu_view_js("-----------{}品のメニュー登録完了-----------".format(count))
         
         print("-----------------------")
         return item_master
@@ -122,14 +106,19 @@ def add_item_master_by_csv(csv_path):
         eel.menu_view_js("-----------マスタ登録が失敗-----------")
         sys.exit()
 
+def main_1(csv_name):#メニューの登録処理
+    ITEM_MASTER_CSV_PATH= './' + csv_name
+    # CSVからマスタへ登録
+    item_master = add_item_master_by_csv(ITEM_MASTER_CSV_PATH) 
 
-def main(csv_name,buy_item_code,buy_item_count,change_money):    
-    item_master=add_item_master_by_csv(ITEM_MASTER_CSV_PATH) # CSVからマスタへ登録
+def main_2(buy_item_code,buy_item_count):#商品番号&個数入力する処理
     order=Order(item_master)
-    order.input_order()    
+    order.input_order(buy_item_code,buy_item_count)
 
+def main_3():#購入する商品番号&個数表示する処理
+    order=Order(item_master)
     order.view_order() 
-    order.input_change_money()
 
-if __name__ == "__main__":
-    main(csv_name,buy_item_code,buy_item_count,change_money)
+def main_4(change_money):#支払金額入力~お釣り受け取りまでの処理
+    order=Order(item_master)
+    order.input_change_money(change_money)
--- view.py
+++ view.py
@@ -7,8 +7,21 @@ end_point="index.html"
 size=(800,800)
 
 @ eel.expose
-def main(csv_name,buy_item_code,buy_item_count,change_money):
-    search.main(csv_name,buy_item_code,buy_item_count,change_money)
+def main_1(csv_name):
+    search.main_1(csv_name)
+
+@ eel.expose
+def main_2(buy_item_code,buy_item_count):
+    search.main_2(buy_item_code,buy_item_count)
+
+@ eel.expose
+def main_3():
+    search.main_3()
+
+@ eel.expose
+def main_4(pay_money):
+    search.main_4(pay_money)
+
 
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
